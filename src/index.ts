3b073f0c4ec6aea5a71ed1418e496f8d081b238b
Sam <sam.soghomonyan@bk.ru>
2025-01-25 23:54:19 +0400

    project

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 import getTasks from "./tasks";
 import getUsers from "./users";
-import getProjectInfo from "./project";
 async function run(): Promise<void> {
   await getTasks();
   await getUsers();
-  await getProjectInfo();
 }
 
 run();
