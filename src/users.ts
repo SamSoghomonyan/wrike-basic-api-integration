11890953ee8a1c7512ab777ce5e5e38998ad2b0d
Sam <sam.soghomonyan@bk.ru>
2025-01-29 15:40:37 +0400

    users

--- src/users.ts
+++ src/users.ts
@@ -1,39 +1,13 @@
-import dotenv from "dotenv";
-import fs from "fs";
-dotenv.config();
-
-interface Data {
-  id: string;
-  lastName: string;
-  firstName: string;
-  type: string;
-  profiles: [{}];
-  avatarUrl: string;
-  timezone: string;
-  locale: string;
-  deleted: boolean;
-  primaryEmail: string;
-}
-
-interface User {
-  name: string;
-  surname: string;
-  primaryEmail: string;
-  id: string;
-}
+import { fetchData } from "./apiUtils";
+import { User } from "./interfaces";
+import { UsersResponse } from "./interfaces";
 
 const USERS_URL = "https://www.wrike.com/api/v4/contacts";
-const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
 
-async function getUsers() {
-  const response = await fetch(USERS_URL, {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${ACCESS_TOKEN}`,
-    },
-  });
-  const data: { data: Data[] } = await response.json();
-  const users: User[] = data.data.map((user) => {
+export async function getUsers() {
+  const response: UsersResponse = await fetchData(USERS_URL);
+  const data = response.data;
+  const users: User[] = data.map((user) => {
     return {
       id: user.id,
       name: user.firstName,
@@ -43,21 +17,3 @@ async function getUsers() {
   });
   return users;
 }
-
-async function saveToFile(
-  filpath: string,
-  content: string,
-  encoding: BufferEncoding = "utf-8"
-): Promise<void> {
-  return new Promise<void>((resolve, reject) => {
-    fs.writeFile(filpath, content, encoding, (err) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve();
-      }
-    });
-  });
-}
-
-export default getUsers;
