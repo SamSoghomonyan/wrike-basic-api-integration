47bcec4e35847ce9ca6b91b698b1c3d3a545598d
Sam <sam.soghomonyan@bk.ru>
2025-01-29 13:19:32 +0400

    projects

--- src/project.ts
+++ src/project.ts
@@ -1,61 +1,25 @@
-import dotenv from "dotenv";
-import fs from "fs";
+import { fetchData } from "./apiUtils";
+import { Project } from "./interfaces";
+import { ProjectResponse } from "./interfaces";
+const URL = "https://www.wrike.com/api/v4/folders?project=true";
 
-dotenv.config();
+export async function getProjectInfo() {
+  const response: ProjectResponse = await fetchData(URL);
 
-const USERS_URL = "https://www.wrike.com/api/v4/folders";
-const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
+  console.log("response", response);
+  const data = response.data;
+  console.log("data", data);
 
-interface Project {
-  id: string;
-  title: string;
-  authorId: string;
-  ownerIds: string[];
-  customStatusId: string;
-  createdDate: string;
+  const projects: Project[] = data
+    .filter((item) => item.project !== undefined)
+    .map((item) => ({
+      id: item.id,
+      title: item.title,
+      authorId: item.project!.authorId,
+      ownerIds: item.project!.ownerIds,
+      customStatusId: item.project!.customStatusId,
+      createdDate: item.project!.createdDate,
+    }));
+
+  return projects;
 }
-
-interface Data {
-  id: string;
-  title: string;
-  childIds: string[];
-  project?: {
-    authorId: string;
-    ownerIds: string[];
-    customStatusId: string;
-    createdDate: string;
-  };
-}
-
-async function getProjectInfo() {
-  try {
-    const response = await fetch(USERS_URL, {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${ACCESS_TOKEN}`,
-      },
-    });
-
-    if (!response.ok) {
-      throw new Error(`Failed to fetch data: ${response.statusText}`);
-    }
-
-    const data: { data: Data[] } = await response.json();
-    const projects: Project[] = data.data
-      .filter((item) => item.project)
-      .map((item) => ({
-        id: item.id,
-        title: item.title,
-        authorId: item.project!.authorId,
-        ownerIds: item.project!.ownerIds,
-        customStatusId: item.project!.customStatusId,
-        createdDate: item.project!.createdDate,
-      }));
-    return projects;
-    console.log("Projects saved to project.json successfully!");
-  } catch (error) {
-    console.error("Error fetching project data:", error);
-  }
-}
-
-export default getProjectInfo;
